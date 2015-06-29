2b324f75f1f10f612bf4514839e04c9038285c10
Stepan Zastupov <stepan.zastupov@gmail.com>
2015-06-29 15:52:01 +0300

    Better logging

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 from telebot import TeleBot
 
 bot = TeleBot(os.environ["API_TOKEN"])
+logger = logging.getLogger("WhatisBot")
 
 
 @asyncio.coroutine
@@ -23,9 +24,17 @@ def search_wiki(text, lang="en"):
     return (yield from response.json())
 
 
+def get_username(user):
+    parts = ["first_name", "last_name", "username"]
+    title = " ".join(filter(None, (user.get(part) for part in parts)))
+    return title
+
+
 @asyncio.coroutine
 def wiki(message, match, lang):
     text = match.group(2)
+    logger.info("%s:\t%s (%s)", get_username(message["from"]), text, lang)
+
     wiki = yield from search_wiki(text, lang)
     pages = wiki["query"]["pages"]
     for pid, page in pages.items():
@@ -48,7 +57,7 @@ def wiki_ru(message, match):
 def wiki_es(message, match):
     return wiki(message, match, "es")
 
-logging.basicConfig(level=logging.DEBUG, filename="WhatisBot.log")
+logging.basicConfig(level=logging.INFO, filename="WhatisBot.log")
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(bot.loop())
